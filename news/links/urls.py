1ea25d7f6d7792957363c9cfdec279f9ec3127ab
Anubhav722 <anubhavs286@gmail.com>
2017-04-28 01:08:39 +0530

    add userbio update view

--- news/links/urls.py
+++ news/links/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.decorators import login_required as auth
 
-from links.views import LinkListView, UserProfileDetailView
+from links.views import LinkListView, UserProfileDetailView, UserProfileUpdateView
+
+# app_name = 'links'
 urlpatterns = [
     url(r'^$', LinkListView.as_view(), name='home'),
-    url(r'^users/(?P<pk>\d+)/$', UserProfileDetailView.as_view(), name='profile')
+    url(r'^users/(?P<pk>\d+)/$', UserProfileDetailView.as_view(), name='profile'),
+    url(r'^update/(?P<pk>\d+)/$', auth(UserProfileUpdateView.as_view()), name='update')
 ]
--- news/links/views.py
+++ news/links/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, UpdateView
+from django.core.urlresolvers import reverse
 
+from .forms import UserProfileForm
 from .models import Vote, Link, UserProfile
 # Create your views here.
 
@@ -17,3 +19,9 @@ class LinkListView(ListView):
 class UserProfileDetailView(DetailView):
 	model = UserProfile
 	template_name = 'user_detail.html'
+
+class UserProfileUpdateView(UpdateView):
+	model = UserProfile
+	form_class = UserProfileForm
+	template_name = "edit_profile.html"
+	success_url = '/'
