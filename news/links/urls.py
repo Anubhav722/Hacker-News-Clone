258a0ce87ca1478d84ef1e6a4487833935153f7d
Anubhav722 <anubhavs286@gmail.com>
2017-04-28 04:33:57 +0530

    add view, url for link's list and detail page

--- news/links/urls.py
+++ news/links/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import url
 from django.contrib.auth.decorators import login_required as auth
 
-from links.views import LinkListView, UserProfileDetailView, UserProfileUpdateView
+from links.views import (LinkListView, 
+	UserProfileDetailView, 
+	UserProfileUpdateView, 
+	LinkCreateView,
+	LinkDetailView,)
 
 # app_name = 'links'
 urlpatterns = [
     url(r'^$', LinkListView.as_view(), name='home'),
     url(r'^users/(?P<pk>\d+)/$', UserProfileDetailView.as_view(), name='profile'),
-    url(r'^update/(?P<pk>\d+)/$', auth(UserProfileUpdateView.as_view()), name='update')
+    url(r'^update/(?P<pk>\d+)/$', auth(UserProfileUpdateView.as_view()), name='update'),
+    url(r'^create/$', auth(LinkCreateView.as_view()), name='create'),
+    url(r'^detail/(?P<pk>\d+)/$', LinkDetailView.as_view(), name='detail'),
 ]
--- news/links/views.py
+++ news/links/views.py
@@ -2,10 +2,10 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, UpdateView
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
 from django.core.urlresolvers import reverse
 
-from .forms import UserProfileForm
+from .forms import UserProfileForm, LinkForm
 from .models import Vote, Link, UserProfile
 # Create your views here.
 
@@ -25,3 +25,25 @@ class UserProfileUpdateView(UpdateView):
 	form_class = UserProfileForm
 	template_name = "edit_profile.html"
 	success_url = '/'
+
+class LinkCreateView(CreateView):
+	model = Link
+	form_class = LinkForm
+	success_url = '/'
+	template_name = 'link_form.html'
+
+	def form_valid(self, form):
+		f = form.save(commit=False)
+		f.rank_score = 0.0
+		f.submitter = self.request.user
+		f.save()
+
+		return super(CreateView, self).form_valid(form)
+
+class LinkDetailView(DetailView):
+	model = Link
+	template_name = 'link_detail.html'
+
+	# def get_object(self):
+	# 	pk = self.kwargs.get('pk')
+	# 	return Link.objects.get(pk=pk)
